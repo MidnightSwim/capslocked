9d2e65b3b2b29558438f2e51dcd5eb0f46294ec0
JCVH <64169858+MidnightSwim@users.noreply.github.com>
2023-12-29 12:01:58 +0100

    PyInstaller comptability
    - Had to add a function about the path of the picture showed by the program using os library. Tkinter wasn't able to find the .png in the executable package without it.
    - icon added for the .exe

--- capslocked.py
+++ capslocked.py
@@ -1,6 +1,15 @@
 from pynput import keyboard
 from tkinter import *
+import os
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+	
 
 keyList = []
 
@@ -20,7 +29,7 @@ with keyboard.Listener(on_press=on_press) as listener :
 
     window = Tk()
     window.overrideredirect(True)
-    picture = PhotoImage(file='caps_lock_128.png')
+    picture = PhotoImage(file=resource_path('caps_lock_128.png'))
     window.title("Capslocked")
     window.withdraw()
     label = Label(window, image=picture)
